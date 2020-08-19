cbd4e80e8f532c642c170b40e85e54447a25cbd2
Natalia <natalia.scherma19@outlook.com>
2020-08-18 22:08:29 -0300

    First commit

--- src/MyPack2/Student.java
+++ src/MyPack2/Student.java
@@ -3,13 +3,13 @@ package MyPack2;
 import MyPack1.Person;
 
 public class Student extends Person {
-    private int graduationYear = 2018;
 
-        public void Details() {
+    public void Details() {
             System.out.println("Name: " + fname + " " + lname);
             System.out.println("Email: " + email);
             System.out.println("Age: " + age);
-            System.out.println("Graduation Year: " + graduationYear);
+        int graduationYear = 2018;
+        System.out.println("Graduation Year: " + graduationYear);
         }
     }
 
